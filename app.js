e1d05f338b021f1c5d35eb57ab08f9fe3e7ad868
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2022-11-14 18:00:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// var a = prompt("Enter your first Name");
+// var b = prompt("Enter your second Name");
+// var final = a+b;
+// alert(final);
+
+// var arr = prompt("What is your favorite Phone")
+// var find= arr;
+// alert("Your Favorite Mobile is " + find + " " + "And length is " +  find.length)
