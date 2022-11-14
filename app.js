72e7e6b3c8777228a7ba321614b996c486ac395c
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2022-11-14 19:25:17 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,60 +1,89 @@
-// var a = prompt("Enter your first Name");
-// var b = prompt("Enter your second Name");
-// var final = a+b;
-// alert(final);
+var a = prompt("Enter your first Name");
+var b = prompt("Enter your second Name");
+var final = a+b;
+alert(final);
 
-// var arr = prompt("What is your favorite Phone")
-// var find= arr;
-// alert("Your Favorite Mobile is " + find + " " + "And length is " +  find.length)
+var arr = prompt("What is your favorite Phone")
+var find= arr;
+alert("Your Favorite Mobile is " + find + " " + "And length is " +  find.length)
 
-// var a = "Pakistani";
-// a=a.indexOf("n")
-// console.log(a)
-
-
-// var a = "Hello World"
-// a= a.lastIndexOf("l")
-// console.log(a)
-
-// var a = "Pakistani"
-// a=a.indexOf("i")
-// console.log(a)
-
-// var a = prompt("Enter your first Name");
-// var b = prompt("Enter your second Name");
-// alert("Greeting " + a+" "+b)
-
-// var inp ="Hyderabad"
-// inp=inp.replace("Hydera", "Islama")
-// console.log(inp)
-
-// var inp = "Ali and Sami are best Friend";
-// console.log(inp)
-// inp=inp.replace("and","&")
-// console.log(inp)
+var a = "Pakistani";
+a=a.indexOf("n")
+console.log(a)
 
 
-// var inp = "472";
-// console.log(inp)
+var a = "Hello World"
+a= a.lastIndexOf("l")
+console.log(a)
 
-// var inp = 472;
-// console.log(inp)
+var a = "Pakistani"
+a=a.indexOf("i")
+console.log(a)
 
-// var inp =prompt("Enter a url");
-// console.log(inp)
+var a = prompt("Enter your first Name");
+var b = prompt("Enter your second Name");
+alert("Greeting " + a+" "+b)
 
-// var inp = prompt("Enter a word")
-// inp = inp.toUpperCase()
-// console.log(inp)
-
-// var capital = prompt("Enter a any word of captialize")
-// capital=capital.toLowerCase()
-// console.log(capital)
-
-// var inp = prompt("Enter a any word")
-// inp=inp.toLocaleLowerCase()
-// console.log(inp)
-
-var inp = "35.36";
+var inp ="Hyderabad"
+inp=inp.replace("Hydera", "Islama")
 console.log(inp)
 
+var inp = "Ali and Sami are best Friend";
+console.log(inp)
+inp=inp.replace("and","&")
+console.log(inp)
+
+
+var inp = "472";
+console.log(inp)
+
+var inp = 472;
+console.log(inp)
+
+var inp =prompt("Enter a url");
+console.log(inp)
+
+var inp = prompt("Enter a word")
+inp = inp.toUpperCase()
+console.log(inp)
+
+var capital = prompt("Enter a any word of captialize")
+capital=capital.toLowerCase()
+console.log(capital)
+
+var inp = prompt("Enter a any word")
+inp=inp.toLocaleLowerCase()
+console.log(inp)
+
+var inp = "35.36";
+inp=inp.split('.').join('')
+console.log(inp)
+
+var a  = "3"
+var b = 3
+console.log(a+b)
+
+var a ="3"
+var b = 3
+console.log(a-b)
+
+
+var inp = prompt("Welcome To ABC  Bakery what do you want to order sir/Maam")
+var abc =["cake","applepie","cookie","chips","patties"]
+if(inp == abc){
+    alert(abc + "is avalible at index of")
+}
+var inp1=prompt("Enter a word")
+var inp2=prompt("Enter a word second")
+if(car<cab){
+    console.log("car is greater than cab")
+}
+
+var uni = "University of Karachi"
+for(var i=1;i<uni.length;i++){
+    console.log(uni[i])
+}
+
+var last = "Pakistan"
+last=last.lastIndexOf('');
+console.log(last);
