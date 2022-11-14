22d86ed95fa6bdb3139714ff6c0f8290126e756f
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2022-11-14 18:36:57 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -6,3 +6,55 @@
 // var arr = prompt("What is your favorite Phone")
 // var find= arr;
 // alert("Your Favorite Mobile is " + find + " " + "And length is " +  find.length)
+
+// var a = "Pakistani";
+// a=a.indexOf("n")
+// console.log(a)
+
+
+// var a = "Hello World"
+// a= a.lastIndexOf("l")
+// console.log(a)
+
+// var a = "Pakistani"
+// a=a.indexOf("i")
+// console.log(a)
+
+// var a = prompt("Enter your first Name");
+// var b = prompt("Enter your second Name");
+// alert("Greeting " + a+" "+b)
+
+// var inp ="Hyderabad"
+// inp=inp.replace("Hydera", "Islama")
+// console.log(inp)
+
+// var inp = "Ali and Sami are best Friend";
+// console.log(inp)
+// inp=inp.replace("and","&")
+// console.log(inp)
+
+
+// var inp = "472";
+// console.log(inp)
+
+// var inp = 472;
+// console.log(inp)
+
+// var inp =prompt("Enter a url");
+// console.log(inp)
+
+// var inp = prompt("Enter a word")
+// inp = inp.toUpperCase()
+// console.log(inp)
+
+// var capital = prompt("Enter a any word of captialize")
+// capital=capital.toLowerCase()
+// console.log(capital)
+
+// var inp = prompt("Enter a any word")
+// inp=inp.toLocaleLowerCase()
+// console.log(inp)
+
+var inp = "35.36";
+console.log(inp)
+
